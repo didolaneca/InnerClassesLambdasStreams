d2d7f16f42b6517d7ffb60c899fc172cc0a1a261
Diyan Simeonov <diyan_simeonov@yahoo.com>
2021-04-29 21:47:23 -0500

    Fourth task completed with all tests passing.

--- innerClassesAndLambdas/task4/src/Resume.java
+++ innerClassesAndLambdas/task4/src/Resume.java
@@ -3,17 +3,107 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class Resume {
-/* TODO */
-Same two inner classes and member variables as in task 3.
-    public Resume(Person p){
-/* TODO */
-Same initialization of Person and the List of Education objects
- /* TODO */
-Initialize the priority queue in one line, without importing the Comparator interface.
+/*  Same two inner classes and member variables as in task 3.
+/*  Same initialization of Person and the List of Education objects
+ /* Initialize the priority queue in one line, without importing the Comparator interface.
+
+/* The getter for experience and the methods to add education and experience objects stay the same, as well as the toString()*/
+
+
+
+
+    public class Education {
+        String major, school;
+
+        public Education(String school, String major) {
+            this.major = major;
+            this.school = school;
+        }
+
+        public String getMajor() {
+            return major;
+        }
+
+        public void setMajor(String major) {
+            this.major = major;
+        }
+
+        public String getSchool() {
+            return school;
+        }
+
+        public void setSchool(String school) {
+            this.school = school;
+        }
+
+        @Override
+        public String toString() {
+            return  major + " at " + school;
+        }
     }
-/* TODO */
-The getter for experience and the methods to add education and experience objects stay the same, as well as the toString()
 
+    public static class Experience {
+        String jobTitle;
+        int startYear, toYear;
 
+        public Experience(String jobTitle, int startYear, int toYear) {
+            this.jobTitle = jobTitle;
+            this.startYear = startYear;
+            this.toYear = toYear;
+        }
 
+        @Override
+        public String toString() {
+            return this.startYear + " - " + this.toYear + " : " + this.jobTitle;
+        }
+    }
+
+    /* Create a member variable of type Person (exactly as in task 2)*/
+    private Person person;
+
+    /* Create a List to hold values of type Education (exactly as in task 2) */
+    private List<Education> educationList;
+
+    /* Create a Priority Queue that holds values of type Experience.*/
+    private PriorityQueue<Experience> experienceByPriority;
+    public Resume(Person p){
+        /* Assign a value to the Person variable.
+           Initialize the List with Education objects.
+           Initialize the priority queue with a comparator defined in an anonymous inner class. Check the specs for the comparator.*/
+        this.person = p;
+        this.educationList = new ArrayList<>();
+        this.experienceByPriority = new PriorityQueue<Experience>((experience1, experience2) -> experience2.toYear - experience1.toYear);
+    }
+
+        /* Add a standard getter for the experiences. getExperience. it should return a priority queue.
+        Implement the addEducation, addExperience and override the toString() methods exactly as you did in task 2 (Copy->Paste)*/
+
+    public PriorityQueue<Experience> getExperience() {
+        return this.experienceByPriority;
+    }
+
+    public void addExperience(Experience experience){
+        this.experienceByPriority.add(experience);
+    }
+
+    public void addEducation(Education education){
+        this.educationList.add(education);
+    }
+
+    //Needs to be testes and fixed in proper format
+    @Override
+    public String toString() {
+        StringBuffer result = new StringBuffer()
+                .append(this.person.toString())
+                .append("\nExperience");
+        for (Experience experience : experienceByPriority) {
+            result.append("\n").append(experience);
+        }
+        result
+                .append("\nEducation");
+        for (Education education : educationList) {
+            result.append("\n").append(education);
+        }
+        return result.toString();
+    }
 }
--- innerClassesAndLambdas/task4/src/Task.java
+++ innerClassesAndLambdas/task4/src/Task.java
@@ -1,6 +1,24 @@
 public class Task {
     public static void main(String[] args){
-/* TODO */
-This code is exactly the same as in Task 3 and should yield the same output.
+/* This code is exactly the same as in Task 3 and should yield the same output.*/
+        Person p = new Person("Kim Possible","2000-01-01");
+        p.setPosition("Associate", 34000);
+        Resume r = new Resume(p);
+        /*  Create an education object. School is NEIU, major is Computer Science.*/
+        Resume.Education education = r.new Education("NEIU", "Computer Science");
+
+        /* add this education object to the resume "r"*/
+        r.addEducation(education);
+
+        /* Create a new Experience object and add it to the resume.*/
+        Resume.Experience experience = new Resume.Experience("Microsoft", 1998, 2021);
+        r.addExperience(experience);
+        r.addExperience(/* Create a new experience object on the fly, inside the addExperiece call.*/
+                new Resume.Experience("Google", 1997, 2021)
+        );
+        r.addExperience(
+                new Resume.Experience("NASA", 1996, 2021)
+        );
+        System.out.println(r);
     }
 }
